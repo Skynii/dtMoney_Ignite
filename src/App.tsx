cf3f34064ddc18b4b74ce16775c763ed12ca0cc9
Skynii <ejoice19@gmail.com>
2021-09-07 13:36:15 -0300

    Formulario em Modal com parte visual finalizado.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global"
 import { useState } from 'react';
-import  Modal  from 'react-modal';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');//para modal parecer em qualquer pagina
 
@@ -24,12 +25,9 @@ export function App() {
      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
      <Dashboard />
      
-     <Modal 
-                isOpen={isNewTransactionModalOpen} 
-                onRequestClose={handleCloseNewTransactionModal}
-                >
-                    <h2>Cadastrar Transação</h2>
-                </Modal>
+     <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} />
      <GlobalStyle /> 
     
     </>
